ff6e16af472664a8779f9b8949757c965b1d65fd
Adam DeLessio <adamdelessio@gmail.com>
2020-01-28 12:23:42 -0500

    trying out design ideas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
+import './components/About'
+import './components/Contact'
+// import './components/Home'
+import './components/Projects'
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +16,11 @@ class App extends Component {
   render() {
     return(
       <div className='app-container'>
+        <nav className='nav'>
+          <div className='block1'></div>
+          <div className='block2'></div>
+          <div className='block3'></div>
+        </nav>
 
       </div>
     )
