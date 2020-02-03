f196bc3d522d98470bb0469fc36c64c4528e9783
Adam DeLessio <adamdelessio@gmail.com>
2020-02-03 13:23:24 -0500

    added home page

--- src/App.js
+++ src/App.js
@@ -1,52 +1,20 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
-import './components/About'
-import './components/Contact'
-// import './components/Home'
-import './components/Projects'
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Projects from './components/Projects'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-
-    }
-  }
-
   render() {
-
-    // function getCoors(event) {
-    //   var x = event.clientX
-    //   var y = event.clientY
-    //   let circle = document.querySelector('.circle')
-    //   circle.style.left = x + 'px'
-    //   circle.style.top = y + 'px'
-    // }
-    function handleBlur(event) {
-      let blocks = document.querySelectorAll('.block')
-      blocks.forEach(i => {
-        i.style.animation = 'blur 300ms forwards'
-      })
-      event.target.style.animation = null
-      event.target.style.cursor = 'grab'
-    }  
-    function unblur(event) {
-      let blocks = document.querySelectorAll('.block')
-      blocks.forEach(i => {
-        i.style.animation = 'unblur 300ms forwards'
-      })
-      event.target.style.animation = null
-    }
-
     return(
       <div className='app-container'>
-        <nav className='nav'>
-          <div className='block1 block' onMouseEnter={handleBlur} onMouseLeave={unblur}>About</div>
-          <div className='block2 block' onMouseEnter={handleBlur} onMouseLeave={unblur}>Projects</div>
-          <div className='block3 block' onMouseEnter={handleBlur} onMouseLeave={unblur}>Contact</div>
-        </nav>
-        <div className='circle'></div>
+        <Link to='/' className='home'>Home</Link>
+
+
+        <Route path='/' component={Home} exact />
+        <Route path='About/' component={About} />
 
       </div>
     )
@@ -54,20 +22,3 @@ class App extends Component {
 }
 
 export default App
-
-
-
-
-        // <div className='grid-container'>
-        //   <div className='grid-item'></div>
-        //   <div className='grid-item'></div>
-        //   <div className='grid-item'></div>
-        //   <div className='grid-item'></div>
-        //   <div className='grid-item'></div>
-        //   <div className='grid-item'></div>
-        //   <div className='grid-item'></div>
-        //   <div className='grid-item'></div>
-        //   <div className='grid-item'></div>
-        // </div>
-
-
---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './Home.css'
+import { Link } from 'react-router-dom'
+
+class Home extends Component {
+	render() {
+	    function handleBlur(event) {
+	      let blocks = document.querySelectorAll('.block')
+	      blocks.forEach(i => {
+	        i.style.animation = 'blur 300ms forwards'
+	      })
+	      event.target.style.animation = null
+	      event.target.style.cursor = 'grab'
+	    }  
+	    function unblur(event) {
+	      let blocks = document.querySelectorAll('.block')
+	      blocks.forEach(i => {
+	        i.style.animation = 'unblur 300ms forwards'
+	      })
+	      event.target.style.animation = null
+	    }
+		return(
+			<div className='home-container'>
+		        <nav className='nav'>
+		          <Link to='About/' className='block1 block' onMouseEnter={handleBlur} onMouseLeave={unblur}>About</Link>
+		          <div className='block2 block' onMouseEnter={handleBlur} onMouseLeave={unblur}>Projects</div>
+		          <div className='block3 block' onMouseEnter={handleBlur} onMouseLeave={unblur}>Contact</div>
+		        </nav>
+			</div>
+		)
+	}
+}
+
+export default Home
