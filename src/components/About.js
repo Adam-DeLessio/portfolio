1a366dc7c44722ff24f62d514526e5a549aae4e4
Adam DeLessio <adamdelessio@gmail.com>
2020-02-11 14:14:30 -0500

    added transitions and a gradient

--- src/components/About.js
+++ src/components/About.js
@@ -4,33 +4,39 @@ import './About.css'
 
 class About extends Component {
 	render() {
+		function handleEnter(event) {
+			// event.target.style.animation = 'expand 400ms forwards'
+		}
+		function handleLeave(event) {
+		// 	event.target.style.animation = 'condense 400ms forwards'
+		}
 		return(
 			<div className='about-container'>
 				<main className='grid-container'>
 					<section className='grid-item grid1'>
-						<div className='nav-item nav1'>
+						<div className='nav-item nav1' onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
 							<div className='bullet-container'>
 								<div className='bullet'>
 									<div className='inner-bullet'></div>
 								</div>
 							</div>
-							<div className='name'></div>
+							<div className='name group1'>About</div>
 						</div>
 						<div className='nav-item nav2'>
 							<div className='bullet-container'>
-								<div className='bullet'>
+								<div className='bullet' onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
 									<div className='inner-bullet'></div>
 								</div>
 							</div>
-							<div className='name'></div>
+							<div className='name'>Projects</div>
 						</div>
 						<div className='nav-item nav3'>
 							<div className='bullet-container'>
-								<div className='bullet'>
+								<div className='bullet' onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
 									<div className='inner-bullet'></div>
 								</div>
 							</div>
-							<div className='name'></div>
+							<div className='name'>Contact</div>
 						</div>
 					</section>
 					<section className='grid-item grid2'></section>
