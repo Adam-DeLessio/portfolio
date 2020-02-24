50bc491bce2c4a1211baee0c15cc4312e1c121eb
Adam DeLessio <adamdelessio@gmail.com>
2020-02-23 21:31:58 -0500

    Updates

---
+++ precache-manifest.21ef153b200617ec149c9b966ce6a052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b3d43ec08f65341cdfe3a222e2a7d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6950f0f92efe3f1eb4b6",
+    "url": "/static/css/main.c2719219.chunk.css"
+  },
+  {
+    "revision": "cdf2765f761297025af3",
+    "url": "/static/js/2.b1873a66.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b1873a66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6950f0f92efe3f1eb4b6",
+    "url": "/static/js/main.268ee02f.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
